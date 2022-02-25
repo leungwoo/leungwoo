f7d77361affcbb12dad200f287686ea9c64e90d6
Richard Leung Woo Gabriel <leungwoo@gmail.com>
2022-02-25 15:17:06 -0400

    angular components2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,23 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MenuComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     FlexLayoutModule,
+    MatListModule,
+    MatGridListModule,
+    MatButtonModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/shared/dish.ts
@@ -0,0 +1,30 @@
+export class Dish {
+  public id: string;
+  public name: string;
+  public image: string;
+  public category: string;
+  public featured: boolean;
+  public label: string;
+  public price: string;
+  public description: string;
+
+  constructor(
+    _id: string,
+    _name: string,
+    _image: string,
+    _category: string,
+    _featured: boolean,
+    _label: string,
+    _price: string,
+    _description: string
+  ) {
+    this.id = _id;
+    this.name = _name;
+    this.image = _image;
+    this.category = _category;
+    this.featured = _featured;
+    this.label = _label;
+    this.price = _price;
+    this.description = _description;
+  }
+}
