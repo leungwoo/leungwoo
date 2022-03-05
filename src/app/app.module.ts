0ac81194d779a09ef661a7fe441008911405481a
Richard Leung Woo Gabriel <leungwoo@gmail.com>
2022-03-05 16:38:17 -0400

    HeaderFooter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,8 +13,10 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { DishService } from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 @NgModule({
-  declarations: [AppComponent, MenuComponent, DishdetailComponent],
+  declarations: [AppComponent, MenuComponent, DishdetailComponent, HeaderComponent, FooterComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
